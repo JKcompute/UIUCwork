d2212f825be094d2cabf86c582e52f11d8c85b62
Johnathon Karcz <bigi@wirelessprvnat-172-16-214-248.near.illinois.edu>
2014-02-25 00:49:27 -0600

    Initial Commit

---
+++ find primes/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  find primes
+//
+//  Created by Johnathon Karcz on 2/25/14.
+//  Copyright (c) 2014 Johnathon Karcz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
